f3360f6d83bd57e4a3cdf16fad8aaca5802a94fa
Dhruv Punetha <dhruvpunetha2002@gmail.com>
2021-08-04 01:34:00 +0530

    q1

---
+++ week3/Q1_insertionsort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int ar[10000];
+    int t,co;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        cin>>co;
+        for(int j=0;j<co;j++)
+        {
+            cin>>ar[j];
+        }
+    }
+}
