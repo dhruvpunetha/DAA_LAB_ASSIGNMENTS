3b86ba44f3704d5ebc67197511521ee17c5eb7fa
Dhruv Punetha <dhruvpunetha2002@gmail.com>
2021-10-17 17:27:44 +0530

    p

--- week8/Q2.cpp
+++ week8/Q2.cpp
@@ -1,9 +1,51 @@
 //kruskal algo
 #include<bits/stdc++.h>
 using namespace std;
+int fp(vector<int>p,int i)
+{
+if(p[i]<0)
+return i;
+return fp(p,p[i]);
+}
+bool unionbweig(vector<int> &p, int u, int v)
+{
+    int pu=fp(p,u);
+    int pv=fp(p,v);
+    if(pu!=pv)
+    {
+        if(p[pu]<=p[pv])
+        {
+            p[pu]+=p[pv];
+            p[pv]=pu;
+        }
+        else
+        {
+            p[pv]+=p[pu];
+            p[pu]=pv;
+        }
+        return true;
+    }
+    return false;
+}
 int krus(vector<vector<int>>&v,int n)
 {
-    
+    int ans=0;
+    vector<pair<int,pair<int,int>>> g;
+     for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+        if(v[i][j]!=0)
+        g.push_back(make_pair(v[i][j],make_pair(i,j)));
+     sort(g.begin(),g.end());
+     vector<int>p(n,-1);
+     for(auto i: g)
+     {
+         int u=i.second.first;
+         int v=i.second.second;
+         int w=i.first;
+         if(unionbweig(p,u,v))
+         ans=ans+w;
+     }   
+     return ans;
 }
 int main()
 {
