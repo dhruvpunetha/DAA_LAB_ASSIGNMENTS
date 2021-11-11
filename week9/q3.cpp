586ac61b6773be4ec9b1d0ffcf49da918a309ec2
Dhruv Punetha <dhruvpunetha2002@gmail.com>
2021-11-12 02:08:08 +0530

    dfg

--- week9/q3.cpp
+++ week9/q3.cpp
@@ -6,18 +6,31 @@ int main()
 {
     int n,a,t,ans;
     vector<int>vec;
-    cin>>n;
+  /*  priority_queue <int,vector<int>,greater<int>>mhe;
+    */
+   cin>>n;
     for(int i=0;i<n;i++)
     {
         cin>>t;
         vec.push_back(t);
     }
+     for(int i=0;i<n;i++)
+    /* mhe.push(vec[i]);*/
     sort(vec.begin(),vec.end());
     ans=0;
-    for(int i=1;i<vec.size();i++)
+   for(int i=1;i<vec.size();i++)
     {
         vec[i]=vec[i-1]+vec[i];
         ans+=vec[i];
     }
+    /*while(mhe.size()>1)
+    {
+        int e=mhe.top();
+        mhe.pop();
+        int e2=mhe.top();
+        mhe.pop();
+        ans+=(e+e2);
+        mhe.push(e+e2);
+    }*/
     cout<<ans;
 }
