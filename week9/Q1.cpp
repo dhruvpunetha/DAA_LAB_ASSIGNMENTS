f9e1e692a6449bb108c10b5f2be099d8618e82a0
Dhruv Punetha <dhruvpunetha2002@gmail.com>
2021-11-12 03:09:55 +0530

    floydwarshall

---
+++ week9/Q1.cpp
@@ -0,0 +1,42 @@
+//floydwarshall
+#define INF INT_MAX
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int ar[n][n],ans[n][n];
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        cin>>ar[i][j];
+    if(i!=j&&ar[i][j]==0)
+    ar[i][j]=INF;
+    }
+}
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        for(int k=0;k<n;k++)
+        {
+            if(ar[i][j]>ar[i][k]+ar[k][j]&&ar[i][k]!=INF&&ar[k][j]!=INF)
+            ar[i][j]=ar[i][k]+ar[k][j];
+        }
+    }
+}
+for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(ar[i][j]==INF)
+            cout<<"INF ";
+            else
+        cout<<ar[i][j]<<" ";
+        }
+         cout<<endl;
+    }
+   
+}
