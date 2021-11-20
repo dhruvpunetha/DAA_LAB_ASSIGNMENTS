c2e6fbc935bac888dc91a82ebbf02b53b55a4309
Dhruv Punetha <dhruvpunetha2002@gmail.com>
2021-11-20 16:17:33 +0530

    pushed q1

--- week11/q1.cpp
+++ week11/q1.cpp
@@ -1,21 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+int dp[100][100];
 int mca(int p[],int i,int j)
 {
     if(i==j)
     return 0;
-    int min=INT_MAX;
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    dp[i][j]=INT_MAX;
     int cou;
     for(int k=i;k<j;k++)
     {
         cou=mca(p,i,k)+mca(p,k+1,j)+p[i-1]*p[k]*p[j];
-        if(cou<min)
-        min=cou;
+        if(cou<dp[i][j])
+        dp[i][j]=cou;
     }
-    return min;
+    return dp[i][j];
 }
 int main()
 {
+    memset(dp,-1,sizeof dp);
     int n,a,b;
     cin>>n;
     int ar[n+1];
